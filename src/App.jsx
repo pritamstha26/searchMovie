e34b73a43a4441fc21a36122696ddd7733c515f7
pritamstha <pritamstha26@gmail.com>
2025-12-14 01:04:09 +0545

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { QueryCache, useQuery, useQueryClient } from "@tanstack/react-query";
+import Card from "./components/Card/Card";
 
 export default function App() {
-  return <div>Hello</div>;
+  const fetchData = async () => {
+    const api_key = import.meta.env.VITE_MOVIE_SEARCH_API;
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`
+    );
+    return response.data.results;
+  };
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["dat"],
+    queryFn: fetchData,
+  });
+  console.log(data);
+
+  if (isLoading) return <p>loading</p>;
+  return (
+    <div className=" ">
+      <Card data={data[0]} />;
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const clientQuery = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={clientQuery}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
