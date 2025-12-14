0eecf0d2484a966960d471069c0545cf91ad9a15
pritamstha <pritamstha26@gmail.com>
2025-12-15 00:57:11 +0545

    .

---
+++ src/page/CardList/CardList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import useDataQuery from "../../hooks/useDataQuery";
+import Card from "../../components/Card/Card";
+import { useNavigate } from "react-router-dom";
+export default function CardList() {
+  const { data, isLoading } = useDataQuery("dat");
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/main");
+  };
+  console.log(data);
+  if (isLoading)
+    return (
+      <div className=" h-screen flex justify-center items-center">
+        <p className=" text-7xl">Loading...</p>
+      </div>
+    );
+  return (
+    <div>
+      {data.map((val, index) => {
+        return <Card key={index} data={val} handleClick={handleClick} />;
+      })}
+    </div>
+  );
+}
---
+++ src/page/CardMain/CardMain.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CardMain() {
+  return <div>cardMain</div>;
+}
