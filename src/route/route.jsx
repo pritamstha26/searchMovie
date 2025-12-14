78a9ab866347724a9109cadc70bfc99c05952491
pritamstha <pritamstha26@gmail.com>
2025-12-15 00:54:38 +0545

    feat: add routing

---
+++ src/route/route.jsx
@@ -0,0 +1,23 @@
+import CardList from "../page/CardList/CardList";
+import CardMain from "../page/CardMain/CardMain";
+
+const router = [
+  {
+    path: "/home",
+    element: <CardList />,
+  },
+  {
+    path: "/main",
+    element: <CardMain />,
+  },
+  {
+    path: "/home",
+    element: <CardList />,
+  },
+  {
+    path: "/home",
+    element: <CardList />,
+  },
+];
+
+export default router;
